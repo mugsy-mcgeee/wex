cbf9ac2c8320972f5a5be80b5022e6c6c9cc42b5
Mugsy <mugsy@here.com>
2013-08-28 16:26:30 -0700

    added setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+setup(
+    name = 'wex',
+    packages = ['wex'],
+    version = '0.2',
+    author = 'mugsy',
+    author_email = 'mugsy.mcgeee@gmail.com',
+    url = 'https://github.com/skadistats/wex',
+    description = 'DSL for defining a python object mapping of raw Skadi DOTA2 data',
+    classifiers = [
+      'Programming Language :: Python',
+      'License :: OSI Approved :: MIT License',
+      'Operating System :: OS Independent'
+      ]
+)
